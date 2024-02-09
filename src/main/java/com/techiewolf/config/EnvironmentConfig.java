df7e5e444f66677eaea5091364e92f0e2a8bd4a8
Avishek Behera <avishekbehera87@gmail.com>
2024-02-09 17:55:23 +0530

    (feat): add configuration management feature for environment switching

---
+++ src/main/java/com/techiewolf/config/EnvironmentConfig.java
@@ -0,0 +1,45 @@
+package com.techiewolf.config;
+
+import org.aeonbits.owner.Config;
+
+/**
+ * Owner library makes it painless to manage properties file configuration
+ * Refer documentation for more.
+ */
+@Config.Sources({
+        "file:src/main/resources/environment-config/${environment}.properties",
+})
+public interface EnvironmentConfig extends Config {
+
+  /**
+   * Everytime there is a new key added to Environment properties file
+   * ensure to add it here. Similarly remove from here if there is a need.
+   */
+  @Key("webLoginPageUrl")
+  String webLoginPageUrl();
+
+  @Key("apiBaseUrl")
+  String apiBaseUrl();
+
+  @Key("username")
+  String username();
+
+  @Key("password")
+  String password();
+
+  @Key("avatar")
+  String avatar();
+
+  @Key("login")
+  String login();
+
+  @Key("logout")
+  String logout();
+
+  @Key("users")
+  String users();
+
+  @Key("resetDb")
+  String resetDb();
+
+}
