844b10880750ab08942df03d211fd33e88bab4bd
Avishek Behera <avishekbehera87@gmail.com>
2024-02-15 18:19:47 +0530

    (feat): add allure report dependency and workflow

--- src/test/java/com/techiewolf/tests/unit/FrameworkSanityTest.java
+++ src/test/java/com/techiewolf/tests/unit/FrameworkSanityTest.java
@@ -6,6 +6,10 @@ import com.techiewolf.config.ConfigurationFactory;
 import com.techiewolf.config.EnvironmentConfig;
 import com.techiewolf.config.FrameworkConfig;
 import com.techiewolf.utility.faker.FakerUtil;
+import io.qameta.allure.Description;
+import io.qameta.allure.Link;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.restassured.specification.RequestSpecification;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,6 +20,8 @@ class FrameworkSanityTest {
 
   @Test
   @UnitTest
+  @Description("Test framework config")
+  @Severity(SeverityLevel.BLOCKER)
   void testFrameworkConfig() {
     FrameworkConfig frameworkConfig = ConfigurationFactory.getFrameworkConfig();
     Assertions.assertThat(frameworkConfig.environment()).isNotNull();
@@ -23,12 +29,14 @@ class FrameworkSanityTest {
 
   @Test
   @UnitTest
+  @Severity(SeverityLevel.BLOCKER)
   void testEnvironmentConfig() {
     Assertions.assertThat(environmentConfig.apiBaseUrl()).isNotNull();
   }
 
   @Test
   @UnitTest
+  @Severity(SeverityLevel.BLOCKER)
   void testSpecFactory() {
     RequestSpecification requestSpecification = SpecFactory.getBaseSpec();
     Assertions.assertThat(requestSpecification).isNotNull();
@@ -36,6 +44,8 @@ class FrameworkSanityTest {
 
   @Test
   @UnitTest
+  @Severity(SeverityLevel.BLOCKER)
+  @Link("JIRA-123")
   void testFakerUtil() {
     Assertions.assertThat(FakerUtil.getRandomEmail()).isNotNull();
   }
