7bf79759b22cc6de82f7add8404f395b1f90c8b8
Avishek Behera <avishekbehera87@gmail.com>
2024-02-16 16:01:23 +0530

    (feat): add timing extension to calculate accurate test execution time (#27)

---
+++ src/test/java/com/techiewolf/setup/TestSetup.java
@@ -0,0 +1,7 @@
+package com.techiewolf.setup;
+
+import com.techiewolf.config.extensions.TimingExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(TimingExtension.class)
+public class TestSetup {}
